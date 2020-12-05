4924871ce327644bc545a4418914299745003562
rs845 <rs40@princeton.edu>
2020-12-05 13:17:02 -0500

    models/runmodel w/o densenet results

---
+++ models.py
@@ -0,0 +1,128 @@
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.applications import Xception # TensorFlow ONLY
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras import layers
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+import numpy as np
+import os
+import os.path
+import argparse
+import cv2
+
+
+def build_model(pretrained):
+    model = Sequential([
+        pretrained,
+        layers.GlobalAveragePooling2D(),
+        layers.Dense(1, activation='sigmoid')
+    ])
+    
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer=Adam(),
+        metrics=['accuracy']
+    )
+    model.summary()
+    return model
+
+def build_vgg(pretrained):
+  model = Sequential([pretrained,
+                    layers.Flatten(),
+                    layers.Dense(2, activation = "softmax")])
+  model.layers[0].trainable = False
+  model.compile(loss="binary_crossentropy", optimizer="adam", metrics="accuracy")
+  model.summary()
+  return model
+
+
+def train_dense(training_steps, validation_steps, train_flow, valid_flow):
+    model = DenseNet121(
+        weights= "imagenet",
+        include_top=False,
+        input_shape=(224,224,3)
+    ) 
+    densenet = build_model(model)
+    densenet.summary()
+    checkpoint_path = "results/cp.ckpt"
+
+    # Create a callback that saves the model's weights
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                    save_weights_only=True,
+                                                    verbose=1)
+    densenet.fit(
+        train_flow,
+        epochs = 1,
+        steps_per_epoch = training_steps,
+        validation_data = valid_flow,
+        validation_steps = validation_steps,
+        callbacks = [cp_callback]
+    )
+    return densenet
+
+def train_inception(training_steps, validation_steps, train_flow, valid_flow):
+  model = InceptionV3(
+      include_top=False,
+      weights="imagenet",
+      input_shape=(224,224,3),
+      classifier_activation="softmax",
+  )
+  inceptionnet = build_model(model)
+  inceptionnet.summary()
+  inceptionnet.fit(
+      train_flow,
+      epochs = 1,
+      steps_per_epoch = training_steps,
+      validation_data = valid_flow,
+      validation_steps = validation_steps
+  )
+
+def train_xception(training_steps, validation_steps, train_flow, valid_flow):
+    model = Xception(
+        include_top=False,
+        weights="imagenet",
+        input_shape=(224,224,3),
+        classifier_activation="softmax",
+    )
+    Xceptionnet = build_model(model)
+    Xceptionnet.summary()
+    Xceptionnet.fit(
+        train_flow,
+        epochs = 1,
+        steps_per_epoch = training_steps,
+        validation_data = valid_flow,
+        validation_steps = validation_steps
+    )
+
+def train_VGG(training_steps, validation_steps, train_flow, valid_flow):
+    model = VGG19(
+        include_top = False,
+        weights="imagenet",
+        input_shape=(224,224,3)
+    )
+    print(model.output[-1])
+    VGGmodel = build_vgg(model)
+
+    VGGmodel.fit(
+        train_flow,
+        epochs = 1,
+        steps_per_epoch = training_steps,
+        validation_data = valid_flow,
+        validation_steps = validation_steps
+    )
+
+MODEL_MAP = {
+    "DenseNet" : train_dense,
+    "InceptionNet" : train_inception,
+    "XceptionNet" : train_xception,
+    "VGG" : train_VGG
+}
+
+def train(model_name, train_flow, valid_flow):
+    return MODEL_MAP[model_name](50000//65, 10000//64, train_flow, valid_flow)
--- runmodel.py
+++ runmodel.py
@@ -6,52 +6,235 @@ from tensorflow.keras.callbacks import Callback, ModelCheckpoint
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-import matplotlib.pyplot as plt
+from models import train, build_model
+import argparse
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import time
+import os
+import os.path
+import csv
+import traceback
+import matplotlib.pyplot as plt
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
 from sklearn import metrics
-import tensorflow as tf
-from tqdm import tqdm
+# import tensorflow as tf
+# from tqdm import tqdm
+
+COLUMNS = [
+    "Model",
+    "Image Compression",
+    "Train Image Size",
+    "Test Image Size",
+    "Training Accuracy",
+    "Test Accuracy",
+    "True Negative",
+    "False Positive",
+    "False Negative",
+    "True Positive",
+    "Train Time",
+    "Test Time"
+]
 
 # importing data
+def dataFlow(train_size, test_size):
+    base_path = '/Users/rachelsylwester/Desktop/archive/real_vs_fake/real-vs-fake/'
+    image_gen = ImageDataGenerator(rescale=1./255.)
 
-base_path = '/real-vs-fake/'
-image_gen = ImageDataGenerator(rescale=1./255.)
+    train_flow = image_gen.flow_from_directory(
+        base_path + 'train/',
+        target_size=(train_size, train_size),
+        batch_size=64,
+        class_mode='binary'
+    )
 
-train_flow = image_gen.flow_from_directory(
-    base_path + 'train/',
-    target_size=(224, 224),
-    batch_size=64,
-    class_mode='binary'
-)
+    valid_flow = image_gen.flow_from_directory(
+        base_path + 'valid/',
+        target_size=(train_size, train_size),
+        batch_size=64,
+        class_mode='binary'
+        )
 
-valid_flow = image_gen.flow_from_directory(
-    base_path + 'valid/',
-    target_size=(224, 224),
-    batch_size=64,
-    class_mode='binary'
-)
+    test_flow_nearest = image_gen.flow_from_directory(
+        base_path + 'test/',
+        target_size=(test_size, test_size),
+        batch_size=64,
+        class_mode='binary',
+        interpolation = "nearest"
+    )
 
-test_flow = image_gen.flow_from_directory(
-    base_path + 'test/',
-    target_size=(224, 224),
-    batch_size=64,
-    class_mode='binary'
-)
+    test_flow_box = image_gen.flow_from_directory(
+        base_path + 'test/',
+        target_size=(test_size, test_size),
+        batch_size=64,
+        class_mode='binary',
+        interpolation="box"
+    )
 
+    test_flow_lanczos = image_gen.flow_from_directory(
+        base_path + 'test/',
+        target_size=(test_size, test_size),
+        batch_size=64,
+        class_mode='binary',
+        interpolation="lanczos"
+    )
+
+    test_flow_hamming = image_gen.flow_from_directory(
+        base_path + 'test/',
+        target_size=(test_size, test_size),
+        batch_size=64,
+        class_mode='binary',
+        interpolation="hamming"
+    )
+
+    return train_flow, valid_flow, test_flow_nearest, test_flow_box, test_flow_lanczos, test_flow_hamming
+
+def train_and_test(model_name, compression_type, training_size, test_size):
 # train model
+    if compression_type == "nearest":
+        start = time.process_time()
+        model = train(model_name, train_flow, valid_flow)
+        end = time.process_time()
+    else:
+        start = time.process_time()
+        model = DenseNet121(
+            weights= None,
+            include_top=False,
+            #input_shape=(224,224,3)
+        ) 
+        model = build_model(model)
+        model.load_weights("results/cp.ckpt")
+        end = time.process_time()
+
+    training_time = end-start
+    
+
+    FLOW_MAP = {
+        "nearest": test_flow_nearest,
+        "box": test_flow_box,
+        "lanczos": test_flow_lanczos,
+        "hamming": test_flow_hamming,
+    }
+    test_flow = FLOW_MAP[compression_type]
+
+# save model
+   # np.savez("model_%s.npz" % model_name, **model)
+
+#Evaluate Model
+    
+    # get compression specific test flow
+    # test_flow =
+
+    start = time.process_time()
+    train_pred = model.predict(train_flow, steps=1)
+    train_test = train_flow.classes
+    print(train_pred)
+    train_accuracy = metrics.accuracy_score(train_test, np.round(train_pred))
+    test_pred = model.predict(test_flow, steps=1)
+    test_test = test_flow.classes
+    test_accuracy = metrics.accuracy_score(test_test, np.round(test_pred))
+    print("Sample predictions: ", np.round(test_pred[0:64]))
+    print("Sample actual labels: ", test_test[0:64])
+    test_matrix = metrics.confusion_matrix(test_test, np.round(test_pred)).ravel()
+    end = time.process_time()
+
+    test_time = end-start
 
 
-# save results and model
+    # save results
+    
+    with open("results/%s_results.txt" % model_name, "a") as f:
+        f.write("Model: %s\n" % model_name)
+        f.write("Image Compression: %s\n" % compression)
+        f.write("Train Image Size: %d\n" % training_size)
+        f.write("Test Image Size: %d\n" % test_size)
+        f.write("Train accuracy: %f\n" % train_accuracy)
+        f.write("Test accuracy: %f\n" % test_accuracy)
+        f.write("True Negatives: %f\n" % test_matrix[0])
+        f.write("False Positives: %f\n" % test_matrix[1])
+        f.write("False Negatives: %f\n" % test_matrix[2])
+        f.write("True Positives: %f\n" % test_matrix[3])
+        f.write("Training Time: %s sec\n" % training_time)            
+        f.write("Testing Time: %s sec\n" % test_time)
+        f.write("\n\n")
 
-print("Num training examples: %d" % num_train)
-save_path = "../results/results_%d.csv" % num_train
+    return {
+        "Model" : model_name,
+        "Image Compression" : compression,
+        "Train Image Size" : training_size,
+        "Test Image Size" :  test_size,
+        "Training Accuracy" : train_accuracy,
+        "Test Accuracy" : test_accuracy,
+        "True Negative" : test_matrix[0],
+        "False Positive" : test_matrix[1],
+        "False Negative" : test_matrix[2],
+        "True Positive": test_matrix[3],
+        "Train Time": training_time,
+        "Test Time" : test_time
+    }
 
-# Create new save file if it doesn't exist
-if not os.path.exists(save_path):
-    results = pd.DataFrame(columns=COLUMNS)
-else:
-    results = pd.read_csv(save_path, float_precision='round_trip')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="Only run experiments with this model")
+    # parser.add_argument("--compression", help="Only run experiments with this compression")
+    args = parser.parse_args()
+
+    training_size = 224
+    test_size = 128
+    
+    # initialize dataflow
+    train_flow, valid_flow, test_flow_nearest, test_flow_box, test_flow_lanczos, test_flow_hamming = dataFlow(training_size, test_size)
+
+    models = [
+        "DenseNet",
+        "InceptionNet",
+        "XceptionNet",
+        "VGG"
+    ]
+
+    if args.model not in models:
+        raise Exception("Model not available")
+
+    model_name = args.model
+
+    compressions = [
+        "nearest",
+        "box",
+        "lanczos",
+        "hamming"
+    ]
+
+    # if args.compression not in compressions:
+    #     raise Exception("Compression not available")
+
+    # compression = args.compression
+
+    save_path = "results/results.csv"
+
+    for compression in compressions:
+        # Create new save file if it doesn't exist
+        if not os.path.exists(save_path):
+            results = pd.DataFrame(columns=COLUMNS)
+        else:
+            results = pd.read_csv(save_path, float_precision='round_trip')
+
+        try:
+            results = results.append(train_and_test(model_name, compression, training_size, test_size), ignore_index=True)
+            results.to_csv(save_path, index=False)
+        except Exception as e:
+            print(traceback.format_exc())
+    
+
+
+# print("Num training examples: %d" % num_train)
+# save_path = "../results/results_%d.csv" % num_train
+
+# # Create new save file if it doesn't exist
+# if not os.path.exists(save_path):
+#     results = pd.DataFrame(columns=COLUMNS)
+# else:
+#     results = pd.read_csv(save_path, float_precision='round_trip')
 
 
 
